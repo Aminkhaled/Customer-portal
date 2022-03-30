6b874a2e899fa2d9a576c7715ef596d52c2f91ee
AmenKhaled <aminabdo8@gmail.com>
2022-03-30 09:56:56 +0200

    add invoices table

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InvoicesController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+
+Auth::routes(['register' => false]);
+Route::get('/',function (){
+    return view('auth.login');
+});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
+Route::resource('invoices',InvoicesController::class);
